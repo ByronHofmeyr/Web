1d7d2fa71d08cf1824ea6c7e3193dfc3c592b4aa
ByronHofmeyr <byron@motarik.com>
2018-04-26 07:23:39 +0200

    TicTacToe at start of 2018/04/26

---
+++ TicTacToe/TicTacToe/JavaScript.js
@@ -0,0 +1,233 @@
+﻿var debug = true;
+var userSymbol = "O";
+var compSymbol = "X";
+var yourTurn = true;
+
+
+var TicTacToeObj = {
+    "00": 0, "01": 0, "02": 0,
+    "10": 0, "11": 0, "12": 0,
+    "20": 0, "21": 0, "22": 0
+};
+
+function iWin() {
+    console.log("I Win");
+}
+
+function findEmptyBlock(winRow) {
+    for ()
+}
+
+function decideMove(scoresObj) {
+    var winRow = false;    
+    var moveRow = false;   
+    for (var value in scoresObj) {
+        if (scoresObj.hasOwnProperty(value)) {
+            if (debug) { console.log("value = ", value) };
+            if (value = -2) {
+                winRow = scoreObj.hasOwnProperty(key);
+            } else {
+                if (value = 2) {
+                    moveRow = scoreObj.hasOwnProperty(key);
+                }
+            }
+        }
+    }
+    if (winRow) {
+        emptyBlock = findEmptyBlock(winRow);
+        fillBox(emptyRow, compSymbol);
+        iWin();
+        return;
+    }
+
+/*
+    if (scoreRow == -2) {
+        fillBox(emptyRow, compSymbol);
+        iWin();
+        return;
+    }
+    if (scoreCol == -2) {
+        fillBox(emptyCol, compSymbol);
+        iWin();
+        return;
+    }
+    if (scoreRow == 2) {
+        fillBox(emptyRow, compSymbol);
+        yourTurn = true;
+        return;
+    }
+    //        scoresObj[x + 3] = scoreCol;
+    if (scoreCol == 2) {
+        fillBox(emptyCol, compSymbol);
+        yourTurn = true;
+        return;
+    }
+    scoresObj[7] = scoreDiag1;
+    if (scoreDiag1 == -2) {
+        fillBox(emptyDiag1, compSymbol);
+        iWin();
+        return;
+    }
+
+    scoresObj[8] = scoreDiag2;
+    if (scoreDiag2 == -2) {
+        fillBox(emptyDiag2, compSymbol);
+        iWin();
+        return;
+    }
+    //    scoresObj[7] = scoreDiag1;
+    if (scoreDiag1 == 2) {
+        fillBox(emptyDiag1, compSymbol);
+        yourTurn = true;
+        return;
+    }
+
+    //    scoresObj[8] = scoreDiag2;
+    if (scoreDiag2 == 2) {
+        fillBox(emptyDiag2, compSymbol);
+        yourTurn = true;
+        return;
+    }
+*/
+    if (TicTacToeObj["11"] == 0)
+        fillBox("11", compSymbol);
+    else {
+        fillBox("01", compSymbol);
+    }
+
+}
+
+function computersTurn() {
+    // I assign 1 to the TicTacToeObj for a players mark, and -1 for
+    // a computer mark.
+    // I then score each row, then each column, then each diagonal.
+    // If I find a score of 2 I need to play in that row, column, 
+    // or diagonal.
+    // If I find a score of -2 I need to play that row to win the game.
+
+    var scoresObj = {
+        0: 0, 1: 0, 2: 0,
+        3: 0, 4: 0, 5: 0,
+        6: 0, 7: 0, 8: 0
+    };
+
+    if (debug) { console.log("computersTurn()") };
+    var scoreRow = 0;
+    var scoreCol = 0;
+    var scoreDiag1 = 0;
+    var scoreDiag2 = 0;
+//    var emptyRow = "";
+//    var emptyCol = "";
+//    var emptyDiag1 = "";
+//    var emptyDiag2 = "";
+
+    for (var x = 0; x < 3; x++) {
+        //if (debug) {console.log("x = ", x) };
+        // could check the diagonals here
+        if (TicTacToeObj[x.toString() + x.toString()] == 0) {
+            // score Diagonal 1
+//            emptyDiag1 = (x.toString() + x.toString());
+        } else {
+            scoreDiag1 += TicTacToeObj[x.toString() + x.toString()];
+        }
+
+        if (TicTacToeObj[x.toString() + (2 - x).toString()] == 0) {
+            // score Diagonal 2
+ //           emptyDiag2 = (x.toString() + (2 - x).toString());
+        } else {
+            scoreDiag2 += TicTacToeObj[x.toString() + (2 - x).toString()];
+        }
+
+        for (var y = 0; y < 3; y++) {
+            //if (debug) {console.log("y = ", y) };
+            //if (debug) {console.log("x.toString() + y.toString() = ", (x.toString() + y.toString())) };
+            //if (debug) {console.log("TicTacToeObj[x.toString() + y.toString()] = ", TicTacToeObj[x.toString() + y.toString()]) };
+            if (TicTacToeObj[x.toString() + y.toString()] == 0) {
+                // score Rows
+//                emptyRow = (x.toString() + y.toString());
+            } else {
+                scoreRow += TicTacToeObj[x.toString() + y.toString()];
+            }
+            if (TicTacToeObj[y.toString() + x.toString()] == 0) {
+                // score Columns
+//                emptyCol = (y.toString() + x.toString());
+            } else {
+                scoreCol += TicTacToeObj[y.toString() + x.toString()];
+            }
+        }
+        scoresObj[x] = scoreRow;
+        scoresObj[x+3] = scoreCol;
+
+
+        scoreRow = 0;  // Initialise scoreRow before scoring next row.
+        scoreCol = 0;  // Initialise scoreCol before scoring next col.
+
+    }
+
+    scoresObj[7] = scoreDiag1;
+    scoresObj[8] = scoreDiag2;
+
+    decideMove(scoresObj);
+    yourTurn = true;
+}
+
+function choice(button) {
+    //$("#choice").on('click',function(button){
+    var x = button.id;
+    if (debug) { console.log("choice = ", x); }
+    if (x == "X") {
+        userSymbol = "X";
+        compSymbol = "O";
+    }
+    $("#splash").attr("style", "display:none");
+    $("#post-splash").attr("style", "");
+}
+
+function fillBox(button, symbol) {
+    if (debug) { console.log("button clicked = ", button); };
+    document.getElementById(button).innerText = symbol;
+    if (symbol === userSymbol) {
+        TicTacToeObj[button] = 1;
+    } else {
+        TicTacToeObj[button] = -1;
+    }
+    if (debug) { console.log("TicTacToeObj = ", TicTacToeObj); };
+}
+
+function clearBox(id) {
+    if (debug) { console.log("button to clear = ", id); };
+    document.getElementById(id).innerText = "";
+}
+
+function clearAllBoxes() {
+    TicTacToeObj = {
+        "00": 0, "01": 0, "02": 0,
+        "10": 0, "11": 0, "12": 0,
+        "20": 0, "21": 0, "22": 0
+    };
+
+    for (var key in TicTacToeObj) {
+        if (TicTacToeObj.hasOwnProperty(key)) {
+            clearBox(key);
+        }
+    }
+}
+
+function onClick(button) {
+    var x = button.id;
+    if (yourTurn) {
+        fillBox(x, userSymbol);
+        yourTurn = false;
+        computersTurn();
+    }
+}
+
+$(document).ready(function () {
+
+    reset.addEventListener('click', function () {
+        if (debug) { console.log("click reset"); }
+        clearAllBoxes();
+        yourTurn = true;
+    });
+
+}); 
---
+++ pomodoro/pomodoro/JavaScript.js
@@ -0,0 +1,133 @@
+﻿var debug = false;
+var breakLength = 5;
+var sessionLength = 25;
+var mins;
+var seconds = 60;
+var pause = false;
+var timerRunning = false;
+var sessionActive = true;
+
+var audio = new Audio('http://soundbible.com/mp3/Metal_Gong-Dianakc-109711828.mp3');
+
+function gong() {
+    audio.load();
+    audio.play();
+}
+
+function countdown(minutes) {
+    //var seconds = 60;
+    mins = minutes;
+    function tick() {
+        if (!pause) {   // currently counting down
+            var counter = document.getElementById("countDown");
+            var current_minutes = mins - 1;
+            seconds--;
+            counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+            if (seconds > 0) {
+                setTimeout(tick, 1000);
+            } else {
+                if (mins > 1) {
+                    seconds = 60;
+                    timerRunning = true;
+                    countdown(mins - 1);
+                } else {
+                    var clockName = document.getElementById("session");
+                    var clockColor = document.getElementById("clock");
+                    if (sessionActive) {
+                        gong();
+                        sessionActive = false;
+                        clockName.innerHTML = "BREAK";
+                        clockColor.style.backgroundColor = "#64e544";
+                        seconds = 60;
+                        if (debug) { console.log("breakLength = ", breakLength); }
+                        timerRunning = true;
+                        countdown(breakLength);
+                    } else {
+                        gong();
+                        sessionActive = true;
+                        clockName.innerHTML = "SESSION";
+                        clockColor.style.backgroundColor = "#ddd41c";
+                        seconds = 60;
+                        if (debug) { console.log("breakLength = ", breakLength); }
+                        timerRunning = true;
+                        countdown(sessionLength);
+                    }
+                }
+            }
+        }
+    }
+    tick();
+}
+
+$(document).ready(function () {
+
+    clock.addEventListener('click', function () {
+        if (debug) { console.log("click function"); }
+        if (pause) {
+            if (debug) { console.log("Currently paused counter = ", mins); }
+            pause = false;
+            timerRunning = true;
+            countdown(mins);
+        } else {
+            if (!timerRunning) {
+                if (debug) { console.log("Start timer sessionLength = ", sessionLength); }
+                timerRunning = true;
+                countdown(sessionLength);
+            } else {
+                if (debug) { console.log("Pause timer"); }
+                pause = true;
+            }
+        }
+    });
+
+    $("#reset").on('click', function () {
+        if (debug) { console.log("timerRunning = ", timerRunning); }
+        var clockName = document.getElementById("session");
+        var clockColor = document.getElementById("clock");
+        pause = true;
+        var counter = document.getElementById("countDown");
+        counter.innerHTML = "";
+        mins = sessionLength;
+        clockName.innerHTML = "SESSION";
+        clockColor.style.backgroundColor = "#ddd41c";
+        seconds = 60;
+        timerRunning = false;
+        sessionActive = true;
+    })
+
+    $("#neg-break-length").on('click', function () {
+        if (debug) { console.log("neg-break-length click function"); }
+        if (breakLength > 1) {
+            breakLength--;
+        }
+        var session = document.getElementById("break-length-value");
+        session.innerHTML = breakLength;
+    })
+
+    $("#plus-break-length").on('click', function () {
+        if (debug) { console.log("plus-break-length click function"); }
+        if (breakLength < 99) {
+            breakLength++;
+        }
+        var session = document.getElementById("break-length-value");
+        session.innerHTML = breakLength;
+    })
+
+    $("#neg-session-length").on('click', function () {
+        if (debug) { console.log("neg-session-length click function"); }
+        if (sessionLength > 1) {
+            sessionLength--;
+        }
+        var session = document.getElementById("session-length-value");
+        session.innerHTML = sessionLength;
+    })
+
+    $("#plus-session-length").on('click', function () {
+        if (debug) { console.log("plus-session-length click function"); }
+        if (sessionLength < 99) {
+            sessionLength++;
+        }
+        var session = document.getElementById("session-length-value");
+        session.innerHTML = sessionLength;
+    })
+});
