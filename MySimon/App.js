cf6cd2c9d72232cd362cdea46861bab31582fad1
ByronHofmeyr <byron@motarik.com>
2018-05-23 09:34:15 +0200

    MySimon split into seperate modules

---
+++ MySimon/App.js
@@ -0,0 +1,126 @@
+﻿// This class requires an update(), render() and
+// a handleInput() method.
+/*
+ *  The Player function, which initiates the Player by:
+    Loading the image by setting this.sprite to the appropriate image in the image folder (use the code from the Enemy function as an example on how to do that)
+    Setting the Player initial location
+    The update method for the Player (can be similar to the one for the Enemy)
+    The render method for the Player (use the code from the render method for the Enemy)
+    The handleInput method, which should receive user input, allowedKeys (the key which was pressed) and move the player according to that input. In particular:
+    Left key should move the player to the left, right key to the right, up should move the player up and down should move the player down.
+    Recall that the player cannot move off screen (so you will need to check for that and handle appropriately).
+    If the player reaches the water the game should be reset by moving the player back to the initial location (you can write a separate reset Player method to handle that).
+    You can add your own Player methods as needed.
+ */
+var Player = function () {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    console.log("Player");
+    this.sprite = 'images/char-boy.png';
+    this.x = 2;
+    this.y = 5;
+    //this.speed = 10;
+
+};
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function (dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //console.log("Player.prototype.update");
+};
+
+// Handles collision with the Enemy
+Player.prototype.collision = function () {
+    console.log("Player.prototype.collision");
+
+};
+// Draw the player on the screen, required method for game
+Player.prototype.render = function () {
+    //console.log("Player.prototype.render");
+    //ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+};
+// Handle Input to move player
+Player.prototype.handleInput = function (keyCode) {
+    /*
+     * The handleInput method, which should receive user input, allowedKeys (the key which was pressed) and move the player according to that input. In particular:
+       Left key should move the player to the left, right key to the right, up should move the player up and down should move the player down.
+       Recall that the player cannot move off screen (so you will need to check for that and handle appropriately).
+       If the player reaches the water the game should be reset by moving the player back to the initial location (you can write a separate reset Player method to handle that).
+     */
+    console.log("Player.prototype.handleInput");
+    console.log("keyCode = ", keyCode);
+    switch (keyCode) {
+        case 'left':
+            if (this.x > 0) { this.x--; }
+            break;
+        case 'up':
+            if (this.y > 0) { this.y--; }
+            break;
+        case 'right':
+            if (this.x < 4) { this.x++; }
+            break;
+        case 'down':
+            if (this.y < 5) { this.y++; }
+            break;
+    }
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+console.log("instantiate your objects");
+var amy = new Player();
+amy.y = 1;
+amy.speed = 100;
+    
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // This is the starting point and is called from HTML onload
+    //simon.run = function (canvas) {
+    //    console.log("Simon.run function call");
+
+    //}
+
+    // handle mousemove events
+
+    function handleMouseMove(e) {
+        console.log("handleMouseMove function call");
+        // get mouse position
+        mouseX = parseInt(e.clientX - offsetX);
+        mouseY = parseInt(e.clientY - offsetY);
+
+        // reset the results box to invisible
+        context.clearRect(225, 30, 20, 20);
+
+        // hit-test each arc
+        for (var i = 0; i < arcs.length; i++) {
+
+            // define one arc
+            defineArc(arcs[i]);
+
+            // test that one arc
+            // if "hit" fill the results box with that arc's color
+            if (context.isPointInStroke(mouseX, mouseY)) {
+                context.fillStyle = arcs[i].color;
+                context.fillRect(225, 30, 20, 20);
+                return;
+            }
+
+        }
+
+    }
+
+    console.log("instantiate your objects");
+    // This listens for mouse movements and sends the cordinates to
+    // Player.handleInput() method. 
+    $("#canvas").mousemove(function (e) { handleMouseMove(e); });
+        //player.handleInput(allowedKeys[e.keyCode]);
+    //});
+
---
+++ MySimon/Engine.js
@@ -0,0 +1,248 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Engine.js
+ * This code provides the game loop functionality (update entities and render),
+ * draws the initial game board on the screen, and then calls the update and
+ * render methods.
+ *
+ * A game engine works by drawing the entire game screen over and over, kind of
+ * like a flipbook you may have created as a kid. When your player moves across
+ * the screen, it may look like just that image/character is moving or being
+ * drawn but that is not the case. What's really happening is the entire "scene"
+ * is being drawn over and over, presenting the illusion of animation.
+ *
+ * This engine makes the canvas' context (ctx) object globally available to make 
+ * writing app.js a little simpler to work with.
+ */
+var Engine = (function (global) {
+    /* Predefine the variables we'll be using within this scope,
+     * create the canvas element, grab the 2D context for that canvas
+     * set the canvas elements height/width and add it to the DOM.
+     */
+    var score = 0;
+    var gameOn = false;
+    var strictOn = false;
+    var states = [false, false, false, false];
+    var litcolors = ['red', 'yellow', 'lightblue', 'lightgreen'];
+    var colors = ['darkred', 'darkorange', 'darkblue', 'darkgreen'];
+
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        lastTime;
+
+    canvas.width = 500;
+    canvas.height = 500;
+    doc.body.appendChild(canvas);
+
+    /* This function serves as the kickoff point for the game loop itself
+     * and handles properly calling the update and render methods.
+     */
+    function main() {
+        /* Get our time delta information which is required if your game
+         * requires smooth animation. Because everyone's computer processes
+         * instructions at different speeds we need a constant value that
+         * would be the same for everyone (regardless of how fast their
+         * computer is) - hurray time!
+         */
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0; // was 1000.0
+
+        /* Call our update/render functions, pass along the time delta to
+         * our update function since it may be used for smooth animation.
+         */
+
+        /* Set our lastTime variable which is used to determine the time delta
+         * for the next time this function is called.
+         */
+        lastTime = now;
+
+        /* Use the browser's requestAnimationFrame function to call this
+        * function again as soon as the browser is able to draw another frame.
+        */
+
+        //setTimeout(function () {
+        score++
+        update(dt);
+        render();
+
+        //win.requestAnimationFrame(main);
+        //}, 1000); // How long do you want the delay to be (in milliseconds)?
+
+
+
+    }
+
+    /* This function does some initial setup that should only occur once,
+     * particularly setting the lastTime variable that is required for the
+     * game loop.
+     */
+    function init() {
+        console.log("init function called");
+        reset();
+        lastTime = Date.now();
+        render();
+        main();
+    }
+
+    /* This function is called by main (our game loop) and itself calls all
+     * of the functions which may need to update entity's data. 
+     */
+    function update(dt) {
+        updateEntities(dt);
+    }
+
+    /* This is called by the update function and loops through all of the
+     * objects within your allEnemies array as defined in app.js and calls
+     * their update() methods. It will then call the update function for your
+     * player object. These update methods should focus purely on updating
+     * the data/properties related to the object. Do your drawing in your
+     * render methods.
+     */
+    function updateEntities(dt) {
+        /* ...*/
+        //console.log("updateEntities(dt) function called");
+        //writeMessage3(ctx, 190, 282, "04");
+    }
+
+    /* This function initially draws the "game level", it will then call
+     * the renderEntities function. Remember, this function is called every
+     * game tick (or loop of the game engine) because that's how games work -
+     * they are flipbooks creating the illusion of animation but in reality
+     * they are just drawing the entire screen over and over.
+     */
+    function render() {
+        /* 
+         */
+        //console.log("render function");
+        litquadrant = -1;
+        // Before drawing, clear existing canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+        //writeMessage(ctx, 10, 25, "" + score);
+        writeMessage(ctx, 176, 234, "Simon");
+
+        writeMessage2(ctx, 184, 300, "COUNT");
+        writeMessage2(ctx, 239, 300, "START");
+        writeMessage2(ctx, 285, 300, "STRICT");
+        writeMessage2(ctx, 214, 330, "OFF");
+        writeMessage2(ctx, 278, 330, "ON");
+        writeMessage3(ctx, 190, 282, scoreText(score));
+
+        //Draw a circle red
+        drawCircle(ctx, 252, 275, 8, "red");
+        //Draw a circle yellow
+        drawCircle(ctx, 298, 275, 8, "yellow");
+        //Draw a circle small red
+        if (strictOn && gameOn) {
+            drawCircle(ctx, 298, 258, 3, "red");
+        } else {
+            drawCircle(ctx, 298, 258, 3, "#2a303a");
+        }
+
+        //Draw recangle
+        ctx.fillStyle = '#2a303a';
+        ctx.fillRect(235, 320, 40, 14);
+
+        //Draw recangle
+        ctx.fillStyle = 'lightblue';
+        if (gameOn) {
+            ctx.fillRect(255, 321, 18, 12);
+        } else {
+            ctx.fillRect(236, 321, 18, 12);
+        }
+
+        outerRadius = canvas.width * 0.45;
+        innerRadius = canvas.width * 0.20;
+
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        var i;
+        for (i = 0; i < 4; i++) {
+            states[i] = (litquadrant == i);
+            slice(i);
+        }
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+
+    }
+    var slice = function (quadrant) {
+        ctx.rotate(quadrant * Math.PI / 2);
+        ctx.beginPath();
+        ctx.arc(-5, -5, innerRadius, Math.PI, 3 * Math.PI / 2);
+        //ctx.lineTo(-5, -outerRadius - 5);
+        ctx.arc(-5, -5, outerRadius, 3 * Math.PI / 2, Math.PI, true);
+        //ctx.closePath();
+        if (states[quadrant]) {
+            ctx.fillStyle = litcolors[quadrant];
+        } else {
+            ctx.fillStyle = colors[quadrant];
+        }
+        ctx.fill();
+        ctx.strokeStyle = '#2a303a';
+        ctx.lineWidth = 20;
+        ctx.stroke();
+        //ctx.rotate(-quadrant * Math.PI / 2);
+    }
+    var writeMessage = function (context, x, y, message) {
+        //var context = canvas.getContext('2d');
+        context.clearRect(x, y - 10, canvas.width, 10);
+        context.font = '42pt Pacifico';
+        context.fillStyle = 'black';
+        context.fillText(message, x, y);
+    }
+    var writeMessage2 = function (context, x, y, message) {
+        //var context = canvas.getContext('2d');
+        context.clearRect(x, y - 10, canvas.width, 10);
+        context.font = '8pt Calibri';
+        context.fillStyle = 'black';
+        context.fillText(message, x, y);
+    }
+    var writeMessage3 = function (context, x, y, message) {
+        //var context = canvas.getContext('2d');
+        //console.log("writeMessage3 function called");
+        context.fillRect(x - 5, y - 20, 30, 30);
+        context.font = '15pt Calibri';
+        context.fillStyle = 'red';
+        context.fillText(message, x, y);
+    }
+    var drawCircle = function (context, x, y, radius, color) {
+        //Draw a circle
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI);
+        context.fillStyle = color;
+        context.fill();
+        context.strokeStyle = '#2a303a';
+        context.lineWidth = 2;
+        context.stroke();
+    }
+    var scoreText = function (score) {
+        // convert score to a two diget string
+        if (gameOn) {
+            if (score == 0) {
+                return "---";
+            } else {
+                if (score < 10) {
+                    return "0" + score.toString();
+                } else {
+                    return score.toString();
+                }
+            }
+        } else {
+            return "";
+        }
+
+    }
+
+    /* This function does nothing but it could have been a good place to
+     * handle game reset states - maybe a new game menu or a game over screen
+     * those sorts of things. It's only called once by the init() method.
+     */
+    function reset() {
+        // noop
+    }
+
+    // Call init() to instantiate the game
+    init();
+    //global.ctx = ctx;
+})(this);
+
+
